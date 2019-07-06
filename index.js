535b0692ae0bbcbd19193eff032000b7d0a88f08
Slavik <slavik.saraev@yandex.ru>
2019-07-06 12:29:04 +0300

    initial

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api');
 
-const TOKEN = '887769333:AAG6wgH5qLiNlmzPKYhoIkHmBBkaHLjt1WA'
+const TOKEN = '887769333:AAG6wgH5qLiNlmzPKYhoIkHmBBkaHLjt1WA';
 
-const bot = new TelegramBot(TOKEN, { polling: true })
+const bot = new TelegramBot(TOKEN, { polling: true });
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'HELLO FORM BOT, bot says: "Hi, ${msg.from.first_name}"')
-})
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+const chatID = msg.chat.id;
+const resp = match[1];
+
+bot.sendMessage(chatId,resp);
+});
