b488491337d25e74ff6ac7d7819cb8318f062d8b
Slavik <slavik.saraev@yandex.ru>
2019-07-06 14:57:23 +0300

    initial

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ var options = {
     ]
   })
 };
-bot.onText(/\/start_test/, function (msg, match) {
-  bot.sendMessage(msg.chat.id, 'Выберите любую кнопку:', options);
-});
 
 bot.on('callback_query', function (msg) {
   var answer = msg.data.split('_'); // Делим ответ на две части, превратив в массив. Первый элемент номер вопроса, второй будет вариант ответа.
@@ -22,7 +19,11 @@ bot.on('callback_query', function (msg) {
   var button = answer[1]; // И вариант ответа
 
   // Если присланный вариант совпадает с вариантом из массива
-  if (questions[index].right_answer==button) {
+  if (options[index].right_answer==button) {
     bot.sendMessage(msg.from.id, 'Ответ верный ✅');
   };
 };
+
+bot.onText(/\/start_test/, function (msg, match) {
+  bot.sendMessage(msg.chat.id, 'Выберите любую кнопку:', options);
+});
