6e0d5b02653a4d23134bb5f5d57d466d55e4957c
Slavik <slavik.saraev@yandex.ru>
2019-07-06 15:08:10 +0300

    initial

--- index.js
+++ index.js
@@ -3,27 +3,28 @@ var TOKEN = '887769333:AAG6wgH5qLiNlmzPKYhoIkHmBBkaHLjt1WA';
 
 var bot = new TelegramBot(TOKEN, { polling: true });
 
-var options = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{ text: 'Кнопка 1', callback_data: '0_1' }],
-      [{ text: 'Кнопка 2', callback_data: '0_2' }],
-      [{ text: 'Кнопка 3', callback_data: 'text 3' }]
-    ]
-  })
-};
-
-bot.on('callback_query', function (msg) {
-  var answer = msg.data.split('_'); // Делим ответ на две части, превратив в массив. Первый элемент номер вопроса, второй будет вариант ответа.
-  var index = answer[0]; // Получаем номер вопроса
-  var button = answer[1]; // И вариант ответа
-
-  // Если присланный вариант совпадает с вариантом из массива
-  if (options[index].right_answer==button) {
-    bot.sendMessage(msg.from.id, 'Ответ верный ✅');
-  };
-};
-
-bot.onText(/\/start_test/, function (msg, match) {
-  bot.sendMessage(msg.chat.id, 'Выберите любую кнопку:', options);
+bot.onText(/\/kek/, (msg, match) => {
+	
+	const chatId = msg.chat.id
+	bot.sendMessage(chatId, 'Что выбереш', {
+		reply_markup: {
+			inline_keyboard: [
+			[
+			{
+				text: 'KEKS',
+				callback_data:'KEKS'
+			},
+						{
+				text: 'SHRECK',
+				callback_data:'Shreck'
+			},
+			{
+				text: 'SES',
+				callback_data:'SES'
+			}
+		]
+		]
+		}
+	});
 });
+		
