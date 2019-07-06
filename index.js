fad074adc496aace73cbb9214fe43934b566d9e8
Slavik <slavik.saraev@yandex.ru>
2019-07-06 13:59:58 +0300

    initial

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ var options = {
   })
 };
 
+var options1 = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [{ text: 'Кнопка 1', callback_data: '0_1' }],
+      [{ text: 'Кнопка 2', callback_data: '0_2' }],
+      [{ text: 'Кнопка 3', callback_data: 'text 3' }]
+    ]
+  })
+};
+
 bot.onText(/\/start_test/, function (msg, match) {
   bot.sendMessage(msg.chat.id, 'Выберите любую кнопку:', options);
 });
@@ -25,6 +35,6 @@ bot.on('callback_query', function (msg) {
 
   // Если присланный вариант совпадает с вариантом из массива
   if (questions[index].right_answer==button) {
-    bot.sendMessage(msg.from.id, options);
+    bot.sendMessage(msg.from.id, options1);
   };
 };
