aaf80eabdd59066726da2d338e36794511fbbfd5
Slavik <slavik.saraev@yandex.ru>
2019-07-06 12:59:11 +0300

    initial

--- index.js
+++ index.js
@@ -17,3 +17,17 @@ const chatId = msg.chat.id;
 
 bot.sendMessage(chatId, 'Massage');
 })
+
+var options = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [{ text: 'Кнопка 1', callback_data: '1' }],
+      [{ text: 'Кнопка 2', callback_data: 'data 2' }],
+      [{ text: 'Кнопка 3', callback_data: 'text 3' }]
+    ]
+  })
+};
+
+bot.onText(/\/start_test/, function (msg, match) {
+  bot.sendMessage(msg.chat.id, 'Выберите любую кнопку:', options);
+});
